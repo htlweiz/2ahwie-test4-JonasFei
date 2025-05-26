fbec703adda5c2173e2c221f79f781caacfcb5a7
JonasFei <feiert230068@sr.htlweiz.at>
2025-05-26 08:54:46 +0000

    aufgabe 1 und 2

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
+// Aufgabe 1
+int int_even_odd(int n)
+{
+    if (n == 0)
+        return 0;
+    else if (n % 2 == 0)
+        return 2;
+    else
+        return 1;
+}
+
 int main(int argc, char **argv) {
   printf("This program was called with %d arguments.\n", argc);
   printf("Hello World!\n");
+
+
+// Aufgabe 1
+int test1 = 0, test2 = 7, test3 = 8;
+    printf("int_even_odd(%d) = %d\n", test1, int_even_odd(test1)); 
+    printf("int_even_odd(%d) = %d\n", test2, int_even_odd(test2)); 
+    printf("int_even_odd(%d) = %d\n", test3, int_even_odd(test3)); 
+
+//Aufgabe 2
+int array[10] = {3, 1, 7, 4, 9, 1, 5, 10, -2, 2};
+  for (int i = 0; i < 10; i++)
+  {
+    printf("Zahl %d: %d\n", i + 1, array[i]);
+  }
+
+
+
+
+
+
+
+
+
 }
--- src/programs/c_program_01/main.c
+++ src/programs/c_program_01/main.c
@@ -2,8 +2,3 @@
 
 #include <test_c_lib.h>
 
-int main(int argc, char **argv) {
-  printf("Trying to confirm.\n");
-  say_hello();
-  return 0;
-}

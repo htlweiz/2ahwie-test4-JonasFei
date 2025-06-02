da3c4ab13df1eb5ff3e6931a07e45fa80dbde59a
JonasFei <feiert230068@sr.htlweiz.at>
2025-06-02 09:25:18 +0000

    2.6.2025

--- src/programs/c_program_01/main.c
+++ src/programs/c_program_01/main.c
@@ -2,3 +2,39 @@
 
 #include <test_c_lib.h>
 
+
+void init(int array[], int length, int value)
+{
+    for(int i = 0; i<length; i++)
+    {
+        array[i] = value;
+    }
+}
+
+int main() 
+{
+
+    //function "init(int array[], int length, int value)"
+    //initialisiere ein Array "array" der Länge 5 mit dem Wert 7
+
+    int array[5];
+    
+    init(array, 5, 7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
